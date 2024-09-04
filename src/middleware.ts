dd238fb6f71152109550030e53762e2ebb8d0a85
Jahn Claudio Lim <2022_cete_limjcp@online.htcgsc.edu.ph>
2024-09-04 23:08:56 +0800

    oten

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,99 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const secret = process.env.AUTH_SECRET;
-  const salt = process.env.AUTH_SALT;
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request as any,
+    secret: process.env.AUTH_SECRET,
+  });
 
-  if (!secret) {
-    throw new Error("AUTH_SECRET environment variable is not set");
+  console.log("Token:", token);
+  console.log("Request URL:", request.nextUrl.pathname);
+
+  if (
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/api/auth") ||
+    request.nextUrl.pathname === "/htc-new-seal.png"
+  ) {
+    return NextResponse.next();
   }
 
-  if (!salt) {
-    throw new Error("AUTH_SALT environment variable is not set");
+  if (!token) {
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
   }
 
-  const token = await getToken({ req, secret, salt });
-
-  if (token) {
-    // Check if the user is a student
-    if (token.studentId) {
-      // Redirect to the student dashboard
-      return NextResponse.redirect(new URL("/student-dashboard", req.url));
+  if (request.nextUrl.pathname === "/") {
+    if (token.role && Array.isArray(token.role)) {
+      if (token.role.includes("admin")) {
+        return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+      }
+      if (token.role.includes("staff")) {
+        return NextResponse.redirect(new URL("/staff-dashboard", request.url));
+      }
+      if (token.role.includes("signatory")) {
+        return NextResponse.redirect(
+          new URL("/signatory-dashboard", request.url)
+        );
+      }
+      if (token.role.includes("student")) {
+        return NextResponse.redirect(
+          new URL("/student-dashboard", request.url)
+        );
+      }
     }
   }
 
-  // Continue with the request if no redirection is needed
+  // Authorization checks for specific routes
+  const pathname = request.nextUrl.pathname;
+
+  if (
+    (pathname.startsWith("/register") ||
+      pathname.startsWith("/admin-dashboard")) &&
+    !token.role.includes("admin")
+  ) {
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
+  if (
+    (pathname.startsWith("/staff-dashboard") ||
+      pathname.startsWith("/staff-approve") ||
+      pathname.startsWith("/staff-requirements")) &&
+    !token.role.includes("staff")
+  ) {
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
+  if (
+    (pathname.startsWith("/signatory-dashboard") ||
+      pathname.startsWith("/signatory-sign")) &&
+    !token.role.includes("signatory")
+  ) {
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
+  if (
+    (pathname.startsWith("/student-dashboard") ||
+      pathname.startsWith("/student-clearance") ||
+      pathname.startsWith("/student-requirements") ||
+      pathname.startsWith("/student-clearance/track-clearance")) &&
+    !token.role.includes("student")
+  ) {
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for:
+     * - static files
+     * - API routes
+     * - login page
+     * - logo file
+     * - any other public assets
+     */
+    "/((?!_next/static|_next/image|favicon.ico|login|htc-new-seal.png|unauthorized|api).*)",
+  ],
+};
