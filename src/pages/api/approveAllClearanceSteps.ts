56ceec950593aa2f61c31b82ad444cc436fef46a
Jahn Claudio P. Lim <158127281+limjcp@users.noreply.github.com>
2024-12-04 17:21:07 +0800

    Update approveAllClearanceSteps.ts

--- src/pages/api/approveAllClearanceSteps.ts
+++ src/pages/api/approveAllClearanceSteps.ts
@@ -22,7 +22,7 @@ export default async function handler(
           id: step.stepId,
         },
         data: {
-          status: "APPROVED",
+          status: "COMPLETED",
         },
       })
     );
